3701bc267038f4a447183b8796c1267505f73d12
MRijlaar <mrijlaar@ualberta.ca>
2017-03-19 16:54:39 -0600

    improper merge fix

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import { Route, IndexRoute, browserHistory } from 'react-router';
 
+import React from 'react'
+import { Route, IndexRoute, browserHistory } from 'react-router'
 
-import App from 'components/App';
-import { HomePage, LoginPage, ListsPage, ArticlePage, GroupListsPage, SearchPage } from 'components';
+import App from 'components/App'
+import { HomePage, LoginPage, PersonalListsPage, ArticlePage, GroupListsPage, SearchPage} from 'components'
+
 
 
 const routes = (
-  <Route path="/" component={App}>
-    <IndexRoute component={HomePage} />
-    /*<IndexRoute component={HomePage} />*/
-    <Route path="/personal" component={ListsPage} />
-    <Route path="/group" component={GroupListsPage} />
-    <Route path="/login" component={LoginPage} />
-    <Route path="/search" component={SearchPage} />
-    {/* <Route path='/home' component={HomePage} onEnter={} />*/}
 
+    <Route path='/' component={App}>
+      <IndexRoute component={HomePage}/>
+      /*<IndexRoute component={HomePage}/>*/
+      <Route path='/personal' component={PersonalListsPage}/>
+      <Route path='/group' component={GroupListsPage} />
+      <Route path='/login' component={LoginPage} />
+      <Route path="/search" component={SearchPage} />
+      {/*<Route path='/home' component={HomePage} onEnter={} />*/}
   </Route>
 );
 
