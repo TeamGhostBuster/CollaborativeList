0293e2c59c1c3a79f90b912d6639e9befdc35ffa
Michael Lin <linzichunzf@hotmail.com>
2017-03-22 12:14:46 -0600

    Add dev version and one signal setup

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
 # build mkdocs and deploy to Github Page
 doc:
 	mkdocs gh-deploy
+
+dev:
+	./build-dev.sh
+
+prod:
+	./build.sh
+
---
+++ build-dev.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Init empty cache file
+if [ ! -f .yarn-cache.tgz ]; then
+  echo "Init empty .yarn-cache.tgz"
+  tar cvzf .yarn-cache.tgz --files-from /dev/null
+fi
+
+# Build docker container
+docker build --build-arg GOOGLE_CLIENT_ID=224926533228-4jcfs0862eib0vo9j81b9d6h8agqh30f.apps.googleusercontent.com -t raspberry .
+
+docker run --rm --entrypoint cat raspberry:latest /tmp/yarn.lock > /tmp/yarn.lock
+
+if ! diff -q yarn.lock /tmp/yarn.lock > /dev/null  2>&1; then
+  echo "Saving Yarn cache"
+  docker run --rm --entrypoint tar raspberry:latest czf - /root/.yarn-cache/ > .yarn-cache.tgz
+  echo "Saving yarn.lock"
+  cp /tmp/yarn.lock yarn.lock
+fi
+
+docker-compose -f docker-compose.dev.yml up -d
