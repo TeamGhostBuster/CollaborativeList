ac5796930b9220e3fe170ead9b7c74903bca6b9d
Michael Lin <linzichunzf@hotmail.com>
2017-03-22 12:24:18 -0600

    rename container

--- build-dev.sh
+++ build-dev.sh
@@ -7,13 +7,13 @@ if [ ! -f .yarn-cache.tgz ]; then
 fi
 
 # Build docker container
-docker build --build-arg GOOGLE_CLIENT_ID=224926533228-4jcfs0862eib0vo9j81b9d6h8agqh30f.apps.googleusercontent.com -t raspberry .
+docker build --build-arg GOOGLE_CLIENT_ID=224926533228-4jcfs0862eib0vo9j81b9d6h8agqh30f.apps.googleusercontent.com -t raspberry-dev .
 
-docker run --rm --entrypoint cat raspberry:latest /tmp/yarn.lock > /tmp/yarn.lock
+docker run --rm --entrypoint cat raspberry-dev:latest /tmp/yarn.lock > /tmp/yarn.lock
 
 if ! diff -q yarn.lock /tmp/yarn.lock > /dev/null  2>&1; then
   echo "Saving Yarn cache"
-  docker run --rm --entrypoint tar raspberry:latest czf - /root/.yarn-cache/ > .yarn-cache.tgz
+  docker run --rm --entrypoint tar raspberry-dev:latest czf - /root/.yarn-cache/ > .yarn-cache.tgz
   echo "Saving yarn.lock"
   cp /tmp/yarn.lock yarn.lock
 fi
