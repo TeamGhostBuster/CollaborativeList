22713c9df0a23cad0507dae2bbbe82210832b6b9
Michael Lin <linzichunzf@hotmail.com>
2017-03-23 01:39:47 -0600

    mock data and test utils

---
+++ src/components/mocks/PersonalList/index.js
@@ -0,0 +1,22 @@
+const personalLists = {
+  id: '1',
+  lists: [
+    {
+      id: '1',
+      archived: 'True',
+      name: 'Test List 1'
+    },
+    {
+      id: '2',
+      archived: 'False',
+      name: 'Test List 2'
+    },
+    {
+      id: '3',
+      archived: 'False',
+      name: 'Test List 3'
+    },
+  ]
+};
+
+export default personalLists;
